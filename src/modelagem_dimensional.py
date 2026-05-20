d14ab9732c9b15a70b4d939fcea84ab41c267b68
metatronmcs <metatron.mcs@gmail.com>
2026-05-20 19:02:09 -0300

    Inclusão da estação do ano na dim_calendario

--- src/modelagem_dimensional.py
+++ src/modelagem_dimensional.py
@@ -39,6 +39,17 @@ def criar_modelo_dimensional():
     d_calendario['mes'] = d_calendario['data'].dt.month
     d_calendario['dia'] = d_calendario['data'].dt.day
     d_calendario['dia_semana_nome'] = d_calendario['data'].dt.day_name()
+    def mapear_estacao(mes):
+        if mes in [12, 1, 2]:
+            return 'Verão'
+        elif mes in [3, 4, 5]:
+            return 'Outono'
+        elif mes in [6, 7, 8]:
+            return 'Inverno'
+        else:
+            return 'Primavera'            
+    d_calendario['estacao_ano'] = d_calendario['mes'].apply(mapear_estacao)
+    
     d_calendario.to_csv(output_dir / "dim_calendario.csv", index=False)
 
     # Fato previsão de surf
